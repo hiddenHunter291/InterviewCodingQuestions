8f3f99afadbd58855f9ed3912747b324e48744e8
Siva_Hari <harisiva07112000@gmail.com>
2021-07-14 15:11:34 +0530

    Trie File

---
+++ TrieDataStructure.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Trie {
+public:
+    Trie* children[26];
+    bool isEnd;
+    
+    Trie() {
+        for(Trie* a : this->children)
+            a = NULL;
+        isEnd = false;
+    }
+};
+
+void insertString(Trie* root, string s) {
+    Trie* temp = root;
+    for(char ch : s) {
+        int index = ch - 'a';
+        if(temp->children[index] == NULL) {
+            temp->children[index] = new Trie();
+        }
+        temp = temp->children[index];
+    }
+    temp->isEnd = true;
+}
+
+bool isLeaf(Trie* root) {
+    for(Trie* a : root->children) {
+        if(a != NULL) return false;
+    }
+    return root;
+}
+
+Trie* deleteString(Trie* root, int current_index, string s) {
+    if(root == NULL) return NULL;
+    if(current_index == s.length()) {
+        if(root->isEnd) {
+            root->isEnd = false;
+        }
+        if(isLeaf(root)) {
+            delete (root);
+            return NULL;
+        }
+        return root;
+    }
+    int index = s[current_index] - 'a';
+    root->children[index] = deleteString(root->children[index], current_index + 1, s);
+    if(isLeaf(root) && root->isEnd) {
+        delete (root);
+        root = NULL;
+    }
+    return root;    
+}
+
+bool search(Trie* root, string s) {
+    Trie* temp = root;
+    for(char ch : s) {
+        int index = ch - 'a';
+        if(temp->children[index] == NULL) return false;
+        temp = temp->children[index];
+    }
+    return temp->isEnd;
+}
+
+int main() {
+    Trie* trie = new Trie();
+    insertString(trie, "abc");
+    insertString(trie, "abd");
+    deleteString(trie, 0, "abd");
+    cout << search(trie, "ab") << endl;
+    return 0;
+}
