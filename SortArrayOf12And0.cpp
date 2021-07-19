9826c8016516c5903935609f04d419ff80b4c2c1
Siva_Hari <harisiva07112000@gmail.com>
2021-07-19 13:38:57 +0530

    sort arrays

---
+++ SortArrayOf12And0.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    vector<int> numbers(n);
+    for(int i=0; i<n; ++i) {
+        cin >> numbers[i];
+    }
+    int mid = 0, low = 0, high = n-1;
+    while(mid <= high) {
+        switch(numbers[mid]) {
+            case 0 : 
+                swap(numbers[low], numbers[high]);
+                low++;
+                high++;
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+                swap(numbers[mid], numbers[high]);
+                mid++;
+                high--;
+                break;
+        }
+    }
+    for(int a : numbers) {
+        cout << a << " ";
+    }
+    cout << endl;
+    return 0;
+}
